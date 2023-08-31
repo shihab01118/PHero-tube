464b4e4129b9f53f7bbc2eb58fa5f7aed2984372
shihab01118 <shahed.shihab.0@gmail.com>
2023-09-01 03:59:41 +0600

    error message added when array is empty

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const loadCategories = async () => {
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/categories`);
     const data = await response.json();
     const categories = data.data;
-    
+
     categories.forEach(category => {
         const categoryButton = document.createElement('div');
         categoryButton.innerHTML = `
@@ -21,34 +21,46 @@ const displayCategories = async category_id => {
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${category_id}`);
     const data = await response.json();
     const videos = data.data;
-
-    videos.forEach(video => {
-        console.log(video)
-        const videoCard = document.createElement('div');
-        videoCard.innerHTML = `
-            <div class="card bg-base-100 shadow-xl">
-                <figure><img src=${video.thumbnail} class="h-[200px] w-full"/></figure>
-                <div class="flex gap-3 mt-4">
-                    <div>
-                        <div class="avatar">
-                            <div class="w-10 rounded-full">
-                            <img src=${video.authors[0]?.profile_picture} />
+    console.log(videos)
+    if (videos.length > 0) {
+        videos.forEach(video => {
+            const videoCard = document.createElement('div');
+            videoCard.innerHTML = `
+                <div class="card bg-base-100 shadow-xl">
+                    <figure><img src=${video.thumbnail} class="h-[200px] w-full"/></figure>
+                    <div class="flex gap-3 my-4">
+                        <div>
+                            <div class="avatar">
+                                <div class="w-10 rounded-full">
+                                <img src=${video.authors[0]?.profile_picture} />
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="space-y-2">
-                        <h2 class="card-title">${video.title}</h2>
-                        <div class="flex gap-2 items-center">
-                            <p>${video.authors[0]?.profile_name}</p>
-                            <span>${video.authors[0]?.verified ? "<img src='./images/verified.png'>" : ""}
+                        <div class="space-y-2">
+                            <h2 class="card-title">${video.title}</h2>
+                            <div class="flex gap-2 items-center">
+                                <p>${video.authors[0]?.profile_name}</p>
+                                <span>${video.authors[0]?.verified ? "<img src='./images/verified.png'>" : ""}
+                            </div>
+                            <p>${video.others?.views} views</p>
                         </div>
-                        <p>${video.others?.views} views</p>
                     </div>
                 </div>
+            `;
+            cardContainer.appendChild(videoCard);
+        })
+    }
+    else {
+        const error = document.createElement('div');
+        error.innerHTML = `
+            <div class="text-center">
+                <img src="./images/Icon.png" alt="error" class="block mx-auto">
+                <h2 class="text-[#171717] text-3xl font-bold mt-5">Oops!! Sorry, There is no <br> content here</h2>
             </div>
         `;
-        cardContainer.appendChild(videoCard);
-    })
+        cardContainer.classList = 'mt-12';
+        cardContainer.appendChild(error);
+    }
 }
 
 loadCategories();
