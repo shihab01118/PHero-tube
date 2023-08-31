f41f18925a4df798e3b42911298b46bd73f6a869
shihab01118 <shahed.shihab.0@gmail.com>
2023-09-01 04:53:38 +0600

    got time dynamically

--- js/script.js
+++ js/script.js
@@ -21,14 +21,18 @@ const displayCategories = async category_id => {
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${category_id}`);
     const data = await response.json();
     const videos = data.data;
-    console.log(videos)
     if (videos.length > 0) {
         videos.forEach(video => {
+            const seconds = parseInt(video.others?.posted_date);
+            const hours = parseInt(seconds / 3600);
+            const minutes = parseInt((seconds % 3600) / 60);
+            const time = `${hours} hrs ${minutes} min ago`
+
             const videoCard = document.createElement('div');
             videoCard.innerHTML = `
-                <div class="card bg-base-100 shadow-xl">
-                    <figure><img src=${video.thumbnail} class="h-[200px] w-full"/></figure>
-                    <div class="flex gap-3 my-4">
+                <div class="card bg-base-100 shadow-xl rounded-lg">
+                    <figure><img src=${video.thumbnail} class="h-[180px] w-full rounded-lg"/></figure>
+                    <div class="flex gap-2 mt-4 mb-4 ps-2">
                         <div>
                             <div class="avatar">
                                 <div class="w-10 rounded-full">
@@ -36,17 +40,18 @@ const displayCategories = async category_id => {
                                 </div>
                             </div>
                         </div>
-                        <div class="space-y-2">
+                        <div>
                             <h2 class="card-title">${video.title}</h2>
-                            <div class="flex gap-2 items-center">
+                            <div class="flex gap-1 items-center card-text mt-1 mb-2">
                                 <p>${video.authors[0]?.profile_name}</p>
                                 <span>${video.authors[0]?.verified ? "<img src='./images/verified.png'>" : ""}
                             </div>
-                            <p>${video.others?.views} views</p>
+                            <p class="card-text">${video.others?.views} views</p>
                         </div>
                     </div>
                 </div>
             `;
+            cardContainer.classList = 'grid md:grid-cols-2 lg:grid-cols-4 gap-6 mt-12 px-8';
             cardContainer.appendChild(videoCard);
         })
     }
@@ -55,7 +60,7 @@ const displayCategories = async category_id => {
         error.innerHTML = `
             <div class="text-center">
                 <img src="./images/Icon.png" alt="error" class="block mx-auto">
-                <h2 class="text-[#171717] text-3xl font-bold mt-5">Oops!! Sorry, There is no <br> content here</h2>
+                <h2 class="text-[#171717] text-2xl md:text-3xl font-bold mt-5">Oops!! Sorry, There is no <br> content here</h2>
             </div>
         `;
         cardContainer.classList = 'mt-12';
